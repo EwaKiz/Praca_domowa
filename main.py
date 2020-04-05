27538c3cff6ec2017c0fefae5a7c0770ab2ab7ac
Ewa <ewakizling@gmail.com>
2020-04-05 23:50:26 +0200

    zaktualizowane

--- main.py
+++ main.py
@@ -8,10 +8,42 @@ Created on Thu Apr  2 23:44:28 2020
 
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
+count = 0
+patients = {}
+
+class Patient(BaseModel):
+    name: str
+    surename: str 
+
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message":  "Hello World during the coronavirus pandemic!"}
+
+
+@app.api_route("/method", methods=["GET", "POST", "DELETE", "PUT"])
+async def method(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient/")
+async def set_patient(patient:Patient):
+    global count, patients
+    count += 1
+    patients[count] = patient
+    return {"id" : count ,"patient": patient}
+
+    
+@app.get("/patient/{pk}")
+async def get_patient(pk:int):
+    global patients
+    if pk in patients.keys():
+        return patients[pk]
+    else:
+        return JSONResponse(status_code=404, content={"message": "Patient not found"})
+
